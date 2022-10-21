2c31339551624acf56cf1c06be3996dd8b6d2356
Prince Kumar <100488688+pk-628996@users.noreply.github.com>
2022-10-21 07:20:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setup(
     download_url=f'https://github.com/pk-628996/pkhelper/archive/refs/tags/v{__version__}-alpha.zip',
     url='https://github.com/pk-628996/pkhelper',
     license="MIT",
-    install_requires=['gdown','pytube','aiohttp','hachoir','cloudscraper','beautifulsoup4'],
+    install_requires=['gdown','pytube','aiohttp','hachoir','cloudscraper','beautifulsoup4','pyshorteners'],
     entry_points={"console_scripts": ["pkhelper=pkhelper.cli:main"]},
 )
