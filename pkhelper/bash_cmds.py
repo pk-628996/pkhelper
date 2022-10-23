b4110255bf3a160dd2900ff47ccd0444cebe6584
Prince Kumar <100488688+pk-628996@users.noreply.github.com>
2022-10-23 06:36:37 +0530

    Update bash_cmds.py

--- pkhelper/bash_cmds.py
+++ pkhelper/bash_cmds.py
@@ -12,13 +12,12 @@ async def bash_async(cmd):
   stdout, stderr = await fetch.communicate()
   result = str(stdout.decode().strip()) \
             + str(stderr.decode().strip())
-  print(result)
   return result
 
 def bash(cmd):
  cmd=cmd.split()
  #r = subprocess.run(['mediainfo', '--output=HTML', file], stdout=subprocess.PIPE).stdout.decode('utf-8')
  r = subprocess.run(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
- stdout,stderr= r.stdout(),r.stderr()
- resu=str(stdout.decode().strip()) \ + str(stderr.decode().strip())
- 
+ stdout,stderr= r.stdout,r.stderr
+ resu=str(stdout.decode().strip()) + str(stderr.decode().strip())
+ return resu
